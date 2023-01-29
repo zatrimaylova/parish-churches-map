87965f637fbd93fa9f6ccb2600611d0d2ecd987a
zatrimaylova <m.zatrimaylova@gmail.com>
2023-01-30 00:10:19 +0300

    added select component

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@
 import React from 'react';
 
 import Button from './ui-kit/button';
+import Select from './ui-kit/select';
 
 import './styles/reset.scss';
+import './styles/styles.scss';
 
 const App = () => {
   return (
     <div className="App">
       <Button title="title" iconName="church" />
+      <Select value="" />
     </div>
   );
 };
