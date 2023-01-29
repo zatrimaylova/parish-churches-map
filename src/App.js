deaca606a05876077c6bc1bc2063c45dbbc9f1b4
zatrimaylova <m.zatrimaylova@gmail.com>
2023-01-29 10:06:47 +0300

    fix prettier errors

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+/**
+ * @prettier
+ */
+
 import React from 'react';
 
 import Button from './ui-kit/button';
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
@@ -7,5 +8,5 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
