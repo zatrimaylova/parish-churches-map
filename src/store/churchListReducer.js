3daceee333f8f645ba26e73e0636a0c5038284d5
zatrimaylova <m.zatrimaylova@gmail.com>
2024-01-04 23:12:34 +0100

    changed churches adding

--- src/store/churchListReducer.js
+++ src/store/churchListReducer.js
@@ -5,7 +5,7 @@ const ADD_CHURCHES = 'ADD_CHURCHES_lIST';
 export const churchesListReducer = (state = defaultState, action) => {
   switch (action.type) {
     case ADD_CHURCHES:
-      return action.payload;
+      return [...state, ...action.payload];
     default:
       return state;
   }
